09aae6da2b8597c14941e8c2fc938c713cb827af
JarnoPauwels <71709292+JarnoPauwels@users.noreply.github.com>
2023-12-17 21:59:07 +0100

    Added Correct Routing

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,15 +1,21 @@
 // src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './HomePage';
-import ProjectPage from './ProjectPage';
+import HomePage from './Components/HomePage';
+import ProjectPage from './Components/ProjectPage';
+import ContactPage from './Components/ContactPage';
+import AboutPage from './Components/AboutPage';
+import ScrollToTop from "./Components/ScrollToTop";
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
         <Route path="/project/:projectId" element={<ProjectPage />} />
       </Routes>
     </Router>
